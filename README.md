e1d066b5f0177b5817d5dfdd810ba11a4040050a
Sofia <sofia.naydenova@student.manchester.ac.uk>
2022-05-04 21:39:05 +0100

    added auth

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/module-two/
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp7a6067c3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
